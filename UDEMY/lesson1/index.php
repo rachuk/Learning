b684db25f2add88644845bf656210f8602571da7
Viktor Rachuk <viktor@MBP-Viktor.Home>
2022-03-30 20:41:45 +0100

    second

---
+++ UDEMY/lesson1/index.php
@@ -0,0 +1,16 @@
+<?php
+    if (mt_rand(0,1)){
+        ?>
+     <div style = "color: blue">Синий текст</div>
+    <?php
+    } else {
+        ?>
+    <div style = "color: red">Красный текст</div>
+    <?php
+}
+
+function getCurrentYear() {
+    print_r(substr);
+}
+getCurrentYear();
+?>
